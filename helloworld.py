e845bef1388fe0e9d9a292213282d56ed4f55f02
AndrewCEmil <andrew.emil@10gen.com>
2013-05-22 18:47:25 -0400

    got it working in a super basic manner

--- helloworld.py
+++ helloworld.py
@@ -6,8 +6,9 @@ def hello_world(request):
     return Response('hello world...')
 
 def basic_page(request):
-    pagesrc = get_page_source()
-    pagesrc.format(get_links())
+    links = get_links()
+    pagesrc = get_page_source(len(links))
+    return Response(pagesrc.format(links[0], links[1], links[2]))
 
 def get_links():
     links = []
@@ -16,15 +17,20 @@ def get_links():
     links.append("http://duckduckgo.com")
     return links
 
-def get_page_source():
-    return "<table><tr><td>Link: <a href=\"{0}\">Link0</a></td><td>Link: <a href=\"{1}\">Link1</a></td><td>Link: <a href=\"{2}\">Link2</a></td></tr></table>"
+def get_page_source(linkcount):
+    elementhtml = "<tr><td>Link: <a href=\"{0}\">Link{1}</a></tr></td>"
+    fullhtml = "<table>"
+    for i in range(linkcount):
+        fullhtml = fullhtml + elementhtml.format("{" + str(i) + "}", i)
+    fullhtml += "</table>"
+    return fullhtml
 
 if __name__ == '__main__':
     config = Configurator()
     config.add_route('hello', '/hello')
     config.add_view(hello_world, route_name='hello')
     config.add_route('links', '/links')
-    config.add_view(get_links, route_name='links')
+    config.add_view(basic_page, route_name='links')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
