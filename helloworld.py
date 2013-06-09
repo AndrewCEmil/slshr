4293065c56d51e926e05c703545997216dc67f9a
AndrewCEmil <andrew.emil@10gen.com>
2013-06-09 00:29:49 -0700

    random commit lol

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,18 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pymongo import Connection
+
+jqinclude = "<script src=\"https://www.google.com/jsapi\"></script><script>google.load('jquery', '1.3.1');</script>"
+
+#post the id of the upvoted link and later the user id
+upvote_script = "<script>
+$.ajax({
+  type:\"POST\", 
+  url:'handle_upvote', 
+  data: {upvote : {0}}, 
+  success: function(data) {alert(data);}});
+</script>"
 
 def hello_world(request):
     return Response('hello world...')
@@ -25,7 +37,10 @@ def get_page_source(linkcount):
     fullhtml += "</table>"
     return fullhtml
 
+def handle_upvote(request):
+    return Response("oh word dude")
 if __name__ == '__main__':
+    conn = Connection()
     config = Configurator()
     config.add_route('hello', '/hello')
     config.add_view(hello_world, route_name='hello')
