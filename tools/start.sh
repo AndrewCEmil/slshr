7d8341425888c8a79f3fc68db3314658105d9d78
Siegfried Bilstein <siegfried@egraphs.com>
2013-07-07 20:54:55 -0700

    added io redirection to nohup

--- tools/start.sh
+++ tools/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kill -TERM $(cat tasks.pid)
 echo 'Starting tasks server'
-nohup ~/env/bin/python ~/app/tasks.py &
+nohup  ~/env/bin/python ~/app/tasks.py > tasks.out 2> tasks.err < /dev/null &
 echo $! > tasks.pid
