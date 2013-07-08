dcc59d653733b3ad8b860eb327ccf9c043372764
Siegfried Bilstein <siegfried@egraphs.com>
2013-07-07 20:22:39 -0700

    forget the & symbol

--- tools/start.sh
+++ tools/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kill $(ps aux | grep 'slshr/app/tasks.py$' | awk '{print $2}')
 echo 'Starting tasks server'
-nohup ~/env/bin/python ~/app/tasks.py 
+nohup ~/env/bin/python ~/app/tasks.py &
